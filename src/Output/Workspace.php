e97adab077048c4dcb38c66d4cbfaca77da15bbc
qualitus-dahme <Dahme@qualitus.de>
2026-08-15 22:23:05 +0200

    feat: add workspace output

--- src/Output/Workspace.php
+++ src/Output/Workspace.php
@@ -12,6 +12,6 @@ final class Workspace implements IOutput {
 
 	public function getOutput(string $out = 'html', bool $final = false): string {
 		// Return simple HTML output
-		return '<h1>Hello Evolution Workspace</h1><p>Evolution can modify existing BASE3 components.</p>';
+		return '<h1>Hello Evolution Workspace</h1><p>Evolution can modify existing BASE3 components.</p><p>This is the second successful Evolution change.</p>';
 	}
 }
